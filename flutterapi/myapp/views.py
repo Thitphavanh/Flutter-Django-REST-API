d416decfd88557acf1a1e0754259f96b91661faa
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-22 22:41:36 +0700

    Update

--- flutterapi/myapp/views.py
+++ flutterapi/myapp/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-data = {'message': 'Hello World'}
+data = [
+    {
+        "title": "Phenomenal",
+        "subtitle": "ເລີ່ມໃຫ້ບໍລິການແຕ່ ປີ 2018 ແມ່ນ Startup platfrom E-Commerce",
+        "imageUrl": "https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/Phenomenal.jpg",
+        "detail": "ແມ່ນບໍລິສັດທີ່ດຳເນີນການດ້ານ platfrom E-Commerce ມີທັງ Application ແລະ Webapplication"
+    },
+    {
+        "title": "Phenomenal Logistic",
+        "subtitle": "ໃຫ້ບໍລິການຂົນສົ່ງລາວ - ຈີນ - ໄທ",
+        "imageUrl": "https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/phenomenal_logistic2.jpg",
+        "detail": "ເລີ່ມດຳເນີນການແມ່ນທ້າຍປີ 2021 ໃຫ້ບໍລິການຂົນສົ່ງລາວ - ຈີນ - ໄທ"
+    },
+    {
+        "title": "Phenomenal Bottega",
+        "subtitle": "ໃຫ້ບໍລິການຂາຍສິນຄ້າ ອອນໄລນ໌ ແບບ E-Commerce",
+        "imageUrl": "https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/Phenomenal%20Bottega.jpg",
+        "detail": "ເລີ່ມດຳເນີນການແມ່ນປີ 2020 ຮັບສັ່ງຊື້ສິນຄ້າ ແລະ ເປັນການຂາຍສິນຄ້າ ອອນໄລນ໌ ແບບ E-Commerce"
+    },
+    {
+        "title": "Phenomenal Book Cloud",
+        "subtitle": "ໃຫ້ບໍລິການຂາຍໜັງສື ອອນໄລນ໌",
+        "imageUrl": "https://raw.githubusercontent.com/Thitphavanh/Flutter-API/main/Phenomenal%20Bookcloud.jpg",
+        "detail": "ເລີ່ມດຳເນີນການແມ່ນປີ 2021 ຂາຍໜັງສືທີ່ບໍ່ມີໜ້າຮ້ານ ເປັນການຂາຍແບບ ອອນໄລນ໌"
+    }
+]
 
 
 def Home(request):
-    return JsonResponse(data=data)
+    return JsonResponse(data=data, safe=False)

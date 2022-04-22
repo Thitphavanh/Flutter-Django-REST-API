749e3711c26ec73b1bb117f882f6570be9a5ef2b
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-22 22:36:53 +0700

    update

--- flutterapi/myapp/views.py
+++ flutterapi/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+data = {'message': 'Hello World'}
+
+
+def Home(request):
+    return JsonResponse(data=data)

50d06c707aaabb1ce0e74a9518a13207cb70b901
Thitphavanh <thitphavanh.psv@gmail.com>
2022-04-26 19:30:29 +0700

    update

--- flutterapi/myapp/models.py
+++ flutterapi/myapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 
-class Todolist(models, Model):
+class Todolist(models.Model):
     title = models.CharField(max_length=100)
     detail = models.TextField(null=True, blank=True)
